2b45887b04c1bce0063f1dd84da3be3b515d0f58
Hamster5295 <hamster5295@163.com>
2024-01-08 18:20:35 +0800

    为 Motivator 增加编辑器UI以显示状态信息

--- Editor/CustomEditor/MotivatorEditor.cs
+++ Editor/CustomEditor/MotivatorEditor.cs
@@ -11,9 +11,23 @@ namespace Motivation.Editor
             base.OnInspectorGUI();
             if (Application.isEditor && Application.isPlaying)
             {
+                var host = target as Motivator;
+
                 EditorGUILayout.Space();
-                if (GUILayout.Button("重新加载模块"))
-                    ((Motivator)target).LoadAllModules();
+                if (GUILayout.Button("重新加载模块", GUILayout.Height(30)))
+                    host.LoadAllModules();
+
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("状态信息(只读)", EditorStyles.boldLabel);
+                EditorGUILayout.Vector2Field("速度", host.Velocity);
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("状态码");
+                var states = MotivationSetting.instance.States;
+                for (int i = 0; i < 32; i++)
+                {
+                    if (!states[i].Enabled) continue;
+                    EditorGUILayout.Toggle(states[i].Name, host.MatchAny(1u << i));
+                }
             }
         }
     }
