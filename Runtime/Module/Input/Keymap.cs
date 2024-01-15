704e3547e5e6d1ef5983f6da873e9573135ab04c
Hamster5295 <hamster5295@163.com>
2024-01-15 21:07:46 +0800

    初步添加 Keymap

---
+++ Runtime/Module/Input/Keymap.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Motivation
+{
+    /// <summary>
+    /// 用于控制按键映射的数据类<br/>
+    /// 这个类构建了一个【用户输入按键】与【控制模块需求按键】的双向映射
+    /// TODO: 支持向鼠标映射
+    /// </summary>
+    public abstract class Keymap : ScriptableObject
+    {
+        /// <summary>
+        /// 将 【用户输入按键】 映射为 【控制模块需求按键】
+        /// </summary>
+        /// <param name="rawKey">用户输入</param>
+        /// <returns>实际提供给控制模块的 KeyCode</returns>
+        public abstract KeyCode GetMappedKey(KeyCode rawKey);
+
+        /// <summary>
+        /// 将 【控制模块需求按键】 映射为 【用户输入按键】<br/>
+        /// 这个方法用于告诉输入模块，究竟需要监听用户输入的哪几个按键
+        /// </summary>
+        /// <param name="mappedKey">控制模块需要的按键</param>
+        /// <returns>用户输入的按键</returns>
+        public abstract KeyCode GetRawKey(KeyCode mappedKey);
+    }
+}
---
+++ Runtime/Module/Input/KeymapInputModule.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using Bingyan;
+using UnityEngine;
+
+namespace Motivation
+{
+    public class KeymapInputModule : InputModule
+    {
+        [SerializeField, Title("按键映射")] protected Keymap keymap;
+
+        protected override void OnKey(KeyCode k)
+            => base.OnKey(keymap.GetMappedKey(k));
+
+        protected override void OnKeyDown(KeyCode k)
+            => base.OnKeyDown(keymap.GetMappedKey(k));
+
+        protected override void OnKeyUp(KeyCode k)
+            => base.OnKeyUp(keymap.GetMappedKey(k));
+
+        public override void InitKeys(IEnumerable<KeyCode> keys)
+            => base.InitKeys(keys.Select(i => keymap.GetRawKey(i)));
+
+        public override void OnKeyAdd(params KeyCode[] addedKeys)
+            => base.OnKeyAdd(addedKeys.Select(i => keymap.GetRawKey(i)).ToArray());
+
+        public override void OnKeyRemove(params KeyCode[] removedKeys)
+            => base.OnKeyRemove(removedKeys.Select(i => keymap.GetRawKey(i)).ToArray());
+    }
+}
