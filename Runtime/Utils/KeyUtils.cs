c184385ac1784807914d00ec18df69c85b5d691a
Hamster5295 <hamster5295@163.com>
2024-03-24 00:41:07 +0800

    增加对 InputSystem 的支持

--- Runtime/Utils/KeyUtils.cs
+++ Runtime/Utils/KeyUtils.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -18,7 +19,7 @@ namespace Motivation
         /// <returns>转化后的 Key</returns>
         public static Key KeyCodeToKey(KeyCode keyCode)
         {
-            if (keycodeToKeyMap.TryGetValue(keyCode, out var val)) return val;
+            if (KeycodeToKeyMap.TryGetValue(keyCode, out var val)) return val;
             throw new Exception($"暂不支持 {keyCode} 转为 Key枚举，请提交 Issue!");
         }
 
@@ -30,11 +31,11 @@ namespace Motivation
         /// <returns>转化后的 KeyCode</returns>
         public static KeyCode KeyToKeyCode(Key key)
         {
-            if (keyToKeyCodeMap.TryGetValue(key, out var val)) return val;
+            if (KeyToKeyCodeMap.TryGetValue(key, out var val)) return val;
             throw new Exception($"暂不支持 {key} 转为 KeyCode枚举，请提交 Issue!");
         }
 
-        private static Dictionary<KeyCode, Key> keycodeToKeyMap = new()
+        public static readonly Dictionary<KeyCode, Key> KeycodeToKeyMap = new()
         {
             { KeyCode.A , Key.A },
             { KeyCode.B , Key.B },
@@ -66,40 +67,55 @@ namespace Motivation
             { KeyCode.Space , Key.Space },
             { KeyCode.Backspace , Key.Backspace },
             { KeyCode.Escape , Key.Escape },
+            { KeyCode.LeftShift , Key.LeftShift },
+            { KeyCode.RightShift , Key.RightShift },
+            { KeyCode.LeftAlt , Key.LeftAlt },
+            { KeyCode.RightAlt , Key.RightAlt },
+            { KeyCode.LeftControl , Key.LeftCtrl },
+            { KeyCode.RightControl , Key.RightCtrl },
+            { KeyCode.LeftCommand , Key.LeftCommand },
+            { KeyCode.RightCommand , Key.RightCommand },
+            { KeyCode.LeftArrow , Key.LeftArrow },
+            { KeyCode.RightArrow , Key.RightArrow },
+            { KeyCode.UpArrow , Key.UpArrow },
+            { KeyCode.DownArrow , Key.DownArrow },
+            { KeyCode.Tab , Key.Tab },
         };
 
-        private static Dictionary<Key, KeyCode> keyToKeyCodeMap = new()
-        {
-            { Key.A , KeyCode.A },
-            { Key.B , KeyCode.B },
-            { Key.C , KeyCode.C },
-            { Key.D , KeyCode.D },
-            { Key.E , KeyCode.E },
-            { Key.F , KeyCode.F },
-            { Key.G , KeyCode.G },
-            { Key.H , KeyCode.H },
-            { Key.I , KeyCode.I },
-            { Key.J , KeyCode.J },
-            { Key.K , KeyCode.K },
-            { Key.L , KeyCode.L },
-            { Key.M , KeyCode.M },
-            { Key.N , KeyCode.N },
-            { Key.O , KeyCode.O },
-            { Key.P , KeyCode.P },
-            { Key.Q , KeyCode.Q },
-            { Key.R , KeyCode.R },
-            { Key.S , KeyCode.S },
-            { Key.T , KeyCode.T },
-            { Key.U , KeyCode.U },
-            { Key.V , KeyCode.V },
-            { Key.W , KeyCode.W },
-            { Key.X , KeyCode.X },
-            { Key.Y , KeyCode.Y },
-            { Key.Z , KeyCode.Z },
-            { Key.Enter , KeyCode.Return },
-            { Key.Space , KeyCode.Space },
-            { Key.Backspace , KeyCode.Backspace },
-            { Key.Escape , KeyCode.Escape },
-        };
+        public static readonly Dictionary<Key, KeyCode> KeyToKeyCodeMap = KeycodeToKeyMap.ToDictionary(i => i.Value, j => j.Key);
+
+        // private static Dictionary<Key, KeyCode> keyToKeyCodeMap = new()
+        // {
+        //     { Key.A , KeyCode.A },
+        //     { Key.B , KeyCode.B },
+        //     { Key.C , KeyCode.C },
+        //     { Key.D , KeyCode.D },
+        //     { Key.E , KeyCode.E },
+        //     { Key.F , KeyCode.F },
+        //     { Key.G , KeyCode.G },
+        //     { Key.H , KeyCode.H },
+        //     { Key.I , KeyCode.I },
+        //     { Key.J , KeyCode.J },
+        //     { Key.K , KeyCode.K },
+        //     { Key.L , KeyCode.L },
+        //     { Key.M , KeyCode.M },
+        //     { Key.N , KeyCode.N },
+        //     { Key.O , KeyCode.O },
+        //     { Key.P , KeyCode.P },
+        //     { Key.Q , KeyCode.Q },
+        //     { Key.R , KeyCode.R },
+        //     { Key.S , KeyCode.S },
+        //     { Key.T , KeyCode.T },
+        //     { Key.U , KeyCode.U },
+        //     { Key.V , KeyCode.V },
+        //     { Key.W , KeyCode.W },
+        //     { Key.X , KeyCode.X },
+        //     { Key.Y , KeyCode.Y },
+        //     { Key.Z , KeyCode.Z },
+        //     { Key.Enter , KeyCode.Return },
+        //     { Key.Space , KeyCode.Space },
+        //     { Key.Backspace , KeyCode.Backspace },
+        //     { Key.Escape , KeyCode.Escape },
+        // };
     }
 }
